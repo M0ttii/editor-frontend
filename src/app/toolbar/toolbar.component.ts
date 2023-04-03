4aed28d73118b4b061fcbbcf2d1ac439fbf99813
h0rzn <hrzn@protonmail.com>
2023-04-03 22:06:22 +0200

    first commit

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Editor } from '@tiptap/core';
 
+
+enum TextModifiers {
+  Bold,
+  Italic
+}
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -9,14 +15,25 @@ import { Editor } from '@tiptap/core';
 export class ToolbarComponent {
   @Input()
   public editor: Editor;
+  mods: typeof TextModifiers = TextModifiers;
+  public activeMod: Number;  
 
   constructor(){
 
   }
 
   public toggleBold(){
-    console.log("Bold")
+    this.activeMod = this.mods.Bold;
     this.editor.chain().focus().toggleBold().run()
   }
 
+  public toggleItalic() {
+    this.activeMod = this.mods.Italic;
+    this.editor.chain().focus().toggleItalic().run()
+  }
+
+  public isModActive(mod: Number) {
+    return this.activeMod == mod
+  }
 }
+
